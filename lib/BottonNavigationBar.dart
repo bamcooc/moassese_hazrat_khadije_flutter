3384391193dc641d7ad5a5d70ded72a708d9fa2c
fhkhd <fhkhdkri.yahoo.com>
2021-09-26 13:20:57 +0330

    add bottomNavigation and webView

---
+++ lib/BottonNavigationBar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'main.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class BottomNavigator extends StatelessWidget {
+  // const BottomNavigator({Key? key}) : super(key: key);
+  late WebViewController _conttroller;
+
+  BottomNavigator(this._conttroller);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: HexColor('011350'),
+      shape: CircularNotchedRectangle(),
+      notchMargin: 10,
+      clipBehavior: Clip.antiAlias,
+      child: Container(
+        height: 60,
+        child:
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width/3,
+              height: 60,
+              child: InkWell(
+                onTap: (){
+
+                },
+                child: Icon(Icons.search_outlined,color: HexColor('5e80c4'),
+              ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width/3,
+              height: 60,
+              child: InkWell(
+                onTap: () async {
+                  final url = await _conttroller.currentUrl();
+                  print('Previous Website: $url');
+                  _conttroller.loadUrl('https://khadije.com/delneveshte');
+                },
+                  child: Icon(Icons.favorite,color: HexColor('5e80c4'),)
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width/3,
+              height: 60,
+              child: InkWell(
+                onTap: ()  {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => HazratKhadije()));
+                },
+                child: Icon(Icons.home_outlined,color: HexColor('5e80c4'),),
+              )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:hazrat_khadije/BottonNavigationBar.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
 
+  debugShowCheckedModeBanner: false,
+  home: HazratKhadije(),
+
+
+  title: "App",
+  localizationsDelegates: [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
+  supportedLocales: [
+    Locale('fa'), // English, no country code
+  ],
+)
+
+);
+
+class HazratKhadije extends StatefulWidget {
+  const HazratKhadije({Key? key}) : super(key: key);
+
+  @override
+  _HazratKhadijeState createState() => _HazratKhadijeState();
+}
+
+class _HazratKhadijeState extends State<HazratKhadije> {
+  late WebViewController conttroller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: WebView(
+        javascriptMode: JavascriptMode.unrestricted,
+        initialUrl: 'https://khadije.com/fa',
+        onWebViewCreated: (controller){
+          this.conttroller = controller;
+        },
+        onPageStarted: (url){
+          print('New website: $url');
+        },
+      ),
+      bottomNavigationBar: BottomNavigator(conttroller),
+    );
+  }
+}
 
