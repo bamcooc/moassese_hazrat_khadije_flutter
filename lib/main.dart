ad82edff479be91ce566789a685d4dc6dfc6102a
fhkhd <fhkhdkri.yahoo.com>
2021-09-27 18:17:18 +0330

    delete bottomNavigationBar.dart and fix home web screen and delneveshte screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hazrat_khadije/BottonNavigationBar.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 
 void main() => runApp(MaterialApp(
 
@@ -38,6 +41,10 @@ class _HazratKhadijeState extends State<HazratKhadije> {
       backgroundColor: Colors.white,
       body: WebView(
         javascriptMode: JavascriptMode.unrestricted,
+        gestureRecognizers: Set()
+          ..add(Factory<VerticalDragGestureRecognizer>(
+                  () => VerticalDragGestureRecognizer())),
+        gestureNavigationEnabled: true,
         initialUrl: 'https://khadije.com/fa',
         onWebViewCreated: (controller){
           this.conttroller = controller;
@@ -46,7 +53,57 @@ class _HazratKhadijeState extends State<HazratKhadije> {
           print('New website: $url');
         },
       ),
-      bottomNavigationBar: BottomNavigator(conttroller),
+      bottomNavigationBar: BottomAppBar(
+        color: HexColor('011350'),
+        shape: CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          height: 60,
+          child:
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width/3,
+                height: 60,
+                child: InkWell(
+                  onTap: (){
+
+                  },
+                  child: Icon(Icons.search_outlined,color: HexColor('5e80c4'),
+                  ),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width/3,
+                height: 60,
+                child: InkWell(
+                    onTap: () async {
+                      final url = await conttroller.currentUrl();
+                      print('Previous Website: $url');
+                      conttroller.loadUrl('https://khadije.com/delneveshte');
+                    },
+                    child: Icon(Icons.favorite,color: HexColor('5e80c4'),)
+                ),
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width/3,
+                  height: 60,
+                  child: InkWell(
+                    onTap: ()  {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => HazratKhadije()));
+                    },
+                    child: Icon(Icons.home_outlined,
+                      color: HexColor('5e80c4'),
+                    ),
+                  )
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
