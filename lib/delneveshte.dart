e1e83d432704e762d9b844869a1575866ae75432
fhkhd <fhkhdkri.yahoo.com>
2021-10-12 12:15:14 +0330

    add circularprogressindiactor to delneveshte screen

--- lib/delneveshte.dart
+++ lib/delneveshte.dart
@@ -11,24 +11,52 @@ class Delneveshte extends StatefulWidget {
 }
 
 class _DelneveshteState extends State<Delneveshte> {
-  WebViewController conttroller;
+  WebViewController _controller;
+  bool isLoading=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebView(
-        javascriptMode: JavascriptMode.unrestricted,
-        gestureRecognizers: Set()
-          ..add(Factory<VerticalDragGestureRecognizer>(
-                  () => VerticalDragGestureRecognizer())),
-        gestureNavigationEnabled: true,
-        initialUrl: 'https://khadije.com/delneveshte',
-        onWebViewCreated: (controller){
-          this.conttroller = controller;
-        },
-        onPageStarted: (url){
-          print('New website: $url');
-        },
-      ),
+      body:       RefreshIndicator(
+        // onRefresh: () async => Future.delayed(Duration(seconds: 3)),
+          onRefresh: () => _controller.reload(),
+          child:
+          Stack(
+              children: <Widget>[
+                WebView(
+                  initialUrl: 'https://khadije.com/delneveshte',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (controller){
+                    this._controller = controller;
+                  },
+                  onPageFinished: (finish) {
+                    setState(() {
+                      // myModel.stopLoading();
+                      isLoading = false;
+                    });
+                  },
+                ),
+                isLoading ? Center( child: CircularProgressIndicator(),)
+                    : Stack(),
+              ]
+          )
+      )
     );
   }
 }
+
+
+
+// WebView(
+// javascriptMode: JavascriptMode.unrestricted,
+// gestureRecognizers: Set()
+// ..add(Factory<VerticalDragGestureRecognizer>(
+// () => VerticalDragGestureRecognizer())),
+// gestureNavigationEnabled: true,
+// initialUrl: 'https://khadije.com/delneveshte',
+// onWebViewCreated: (controller){
+// this.conttroller = controller;
+// },
+// onPageStarted: (url){
+// print('New website: $url');
+// },
+// ),
